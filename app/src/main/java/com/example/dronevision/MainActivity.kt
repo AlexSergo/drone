c76d3f974aa073fa50970eccfbc226548fc9f2fb
danii <daniilgeroldovich@gmail.com>
2022-12-08 13:18:32 +0500

    Add displaying yandex maps

--- app/src/main/java/com/example/dronevision/MainActivity.kt
+++ app/src/main/java/com/example/dronevision/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.dronevision
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dronevision.databinding.ActivityMainBinding
+import com.yandex.mapkit.MapKitFactory
 
 class MainActivity : AppCompatActivity() {
+    
+    private lateinit var binding: ActivityMainBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        MapKitFactory.setApiKey("21d592db-23af-489e-a87f-cf284dd7d62e")
+        MapKitFactory.initialize(this)
+        setContentView(view)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view, MapFragment())
+                .commitNow()
+        }
     }
 }
---
+++ app/src/main/java/com/example/dronevision/MapFragment.kt
@@ -0,0 +1,43 @@
+package com.example.dronevision
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.dronevision.databinding.FragmentMapBinding
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
+
+class MapFragment : Fragment() {
+    
+    private lateinit var binding: FragmentMapBinding
+    
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMapBinding.inflate(inflater, container, false)
+        
+        binding.mapView.map.move(
+            CameraPosition(Point(55.755864, 37.617698), 11.0f, 0.0f, 0.0f),
+            Animation(Animation.Type.SMOOTH, 300f), null
+        )
+        
+        return binding.root
+    }
+    
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+        MapKitFactory.getInstance().onStop()
+    }
+    
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+        MapKitFactory.getInstance().onStart()
+    }
+}
