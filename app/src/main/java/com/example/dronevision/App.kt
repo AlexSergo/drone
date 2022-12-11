8edbc8adf3966aba5f1f05958655d07720c1384c
danii <daniilgeroldovich@gmail.com>
2022-12-11 12:29:28 +0500

    Add nav drawer menu

---
+++ app/src/main/java/com/example/dronevision/App.kt
@@ -0,0 +1,12 @@
+package com.example.dronevision
+
+import android.app.Application
+
+class App : Application() {
+    
+    /*override fun onCreate() {
+        super.onCreate()
+        MapKitFactory.setApiKey("21d592db-23af-489e-a87f-cf284dd7d62e")
+        MapKitFactory.initialize(this)
+    }*/
+}
--- app/src/main/java/com/example/dronevision/MainActivity.kt
+++ app/src/main/java/com/example/dronevision/MainActivity.kt
@@ -1,25 +1,69 @@
 package com.example.dronevision
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.dronevision.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import com.yandex.mapkit.MapKitFactory
 
 class MainActivity : AppCompatActivity() {
     
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
+    
         MapKitFactory.setApiKey("21d592db-23af-489e-a87f-cf284dd7d62e")
         MapKitFactory.initialize(this)
+    
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
         setContentView(view)
-        if (savedInstanceState == null) {
+    
+        /*if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container_view, MapFragment())
+                .replace(R.id.nav_host_fragment_content_main, MapFragment())
                 .commitNow()
-        }
+        }*/
+        setSupportActionBar(binding.appBarMain.toolbar)
+    
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.targ01, R.id.targ04, R.id.targ08,
+                R.id.targ10, R.id.targ12, R.id.targ14,
+                R.id.targ17, R.id.targ19, R.id.targ20,
+                R.id.targ21, R.id.targ22, R.id.targ23,
+                R.id.targ24, R.id.targ25, R.id.targ27,
+                R.id.targ29, R.id.targ30, R.id.targ31,
+                R.id.targ99, R.id.breach
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
+    
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+    
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+    
 }
