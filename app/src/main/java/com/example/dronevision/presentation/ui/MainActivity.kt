5db6091cb43c56a37d8ff87b57673eb0eccf1d09
danii <daniilgeroldovich@gmail.com>
2022-12-15 03:05:52 +0500

    Add adding point to the map

--- app/src/main/java/com/example/dronevision/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/dronevision/presentation/ui/MainActivity.kt
@@ -34,15 +34,17 @@ import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
+import com.yandex.runtime.image.ImageProvider
 import java.io.IOException
 
-class MainActivity : AppCompatActivity(), BluetoothReceiver.MessageListener {
+class MainActivity : AppCompatActivity(), BluetoothReceiver.MessageListener,
+    NavigationView.OnNavigationItemSelectedListener {
     
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
     private lateinit var locationRequest: LocationRequest
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-
+    
     lateinit var bluetoothConnection: BluetoothConnection
     private var dialog: SelectBluetoothFragment? = null
     
@@ -62,10 +64,9 @@ class MainActivity : AppCompatActivity(), BluetoothReceiver.MessageListener {
     
     private fun initMap() {
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-        binding.mapView.map.move(
-            CameraPosition(Point(55.755864, 37.617698), 11.0f, 0.0f, 0.0f),
-            Animation(Animation.Type.SMOOTH, 300f), null
-        )
+    
+        // Создание пользователя на карте пока не отображаем ибо надо TODO: создать отдельное активити с запросами на разрешение
+//        MapKitFactory.getInstance().createUserLocationLayer(binding.mapView.mapWindow).isVisible = true
     }
     
     private fun setupNavController() {
@@ -74,28 +75,195 @@ class MainActivity : AppCompatActivity(), BluetoothReceiver.MessageListener {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        
+    
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.targ01, R.id.targ04, R.id.targ08,
-                R.id.targ10, R.id.targ12, R.id.targ14,
-                R.id.targ17, R.id.targ19, R.id.targ20,
-                R.id.targ21, R.id.targ22, R.id.targ23,
-                R.id.targ24, R.id.targ25, R.id.targ27,
-                R.id.targ29, R.id.targ30, R.id.targ31,
-                R.id.targ99, R.id.breach
+                R.id.targ01, R.id.targ04, R.id.targ08, R.id.targ10, R.id.targ12, R.id.targ14,
+                R.id.targ17, R.id.targ19, R.id.targ20, R.id.targ21, R.id.targ22, R.id.targ23,
+                R.id.targ24, R.id.targ25, R.id.targ27, R.id.targ29, R.id.targ30, R.id.targ31,
+                R.id.breach, R.id.targ99
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    
+        navView.setNavigationItemSelectedListener(this)
+    }
+    
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.targ01 -> {
+                val cameraPositionTarget = binding.mapView.map.cameraPosition.target
+                val mapObjCollection = binding.mapView.map.mapObjects.addCollection()
+                mapObjCollection.addPlacemark(
+                    Point(cameraPositionTarget.latitude, cameraPositionTarget.longitude),
+                    ImageProvider.fromResource(applicationContext, R.drawable.ic_01)
+                )
+            }
+            R.id.targ04 -> {
+                val cameraPositionTarget = binding.mapView.map.cameraPosition.target
+                val mapObjCollection = binding.mapView.map.mapObjects.addCollection()
+                mapObjCollection.addPlacemark(
+                    Point(cameraPositionTarget.latitude, cameraPositionTarget.longitude),
+                    ImageProvider.fromResource(applicationContext, R.drawable.ic_04)
+                )
+            }
+            R.id.targ08 -> {
+                val cameraPositionTarget = binding.mapView.map.cameraPosition.target
+                val mapObjCollection = binding.mapView.map.mapObjects.addCollection()
+                mapObjCollection.addPlacemark(
+                    Point(cameraPositionTarget.latitude, cameraPositionTarget.longitude),
+                    ImageProvider.fromResource(applicationContext, R.drawable.ic_08)
+                )
+            }
+            R.id.targ10 -> {
+                val cameraPositionTarget = binding.mapView.map.cameraPosition.target
+                val mapObjCollection = binding.mapView.map.mapObjects.addCollection()
+                mapObjCollection.addPlacemark(
+                    Point(cameraPositionTarget.latitude, cameraPositionTarget.longitude),
+                    ImageProvider.fromResource(applicationContext, R.drawable.ic_10)
+                )
+            }
+            R.id.targ12 -> {
+                val cameraPositionTarget = binding.mapView.map.cameraPosition.target
+                val mapObjCollection = binding.mapView.map.mapObjects.addCollection()
+                mapObjCollection.addPlacemark(
+                    Point(cameraPositionTarget.latitude, cameraPositionTarget.longitude),
+                    ImageProvider.fromResource(applicationContext, R.drawable.ic_12)
+                )
+            }
+            R.id.targ14 -> {
+                val cameraPositionTarget = binding.mapView.map.cameraPosition.target
+                val mapObjCollection = binding.mapView.map.mapObjects.addCollection()
+                mapObjCollection.addPlacemark(
+                    Point(cameraPositionTarget.latitude, cameraPositionTarget.longitude),
+                    ImageProvider.fromResource(applicationContext, R.drawable.ic_14)
+                )
+            }
+            R.id.targ17 -> {
+                val cameraPositionTarget = binding.mapView.map.cameraPosition.target
+                val mapObjCollection = binding.mapView.map.mapObjects.addCollection()
+                mapObjCollection.addPlacemark(
+                    Point(cameraPositionTarget.latitude, cameraPositionTarget.longitude),
+                    ImageProvider.fromResource(applicationContext, R.drawable.ic_17)
+                )
+            }
+            R.id.targ19 -> {
+                val cameraPositionTarget = binding.mapView.map.cameraPosition.target
+                val mapObjCollection = binding.mapView.map.mapObjects.addCollection()
+                mapObjCollection.addPlacemark(
+                    Point(cameraPositionTarget.latitude, cameraPositionTarget.longitude),
+                    ImageProvider.fromResource(applicationContext, R.drawable.ic_19)
+                )
+            }
+            R.id.targ20 -> {
+                val cameraPositionTarget = binding.mapView.map.cameraPosition.target
+                val mapObjCollection = binding.mapView.map.mapObjects.addCollection()
+                mapObjCollection.addPlacemark(
+                    Point(cameraPositionTarget.latitude, cameraPositionTarget.longitude),
+                    ImageProvider.fromResource(applicationContext, R.drawable.ic_20)
+                )
+            }
+            R.id.targ21 -> {
+                val cameraPositionTarget = binding.mapView.map.cameraPosition.target
+                val mapObjCollection = binding.mapView.map.mapObjects.addCollection()
+                mapObjCollection.addPlacemark(
+                    Point(cameraPositionTarget.latitude, cameraPositionTarget.longitude),
+                    ImageProvider.fromResource(applicationContext, R.drawable.ic_21)
+                )
+            }
+            R.id.targ22 -> {
+                val cameraPositionTarget = binding.mapView.map.cameraPosition.target
+                val mapObjCollection = binding.mapView.map.mapObjects.addCollection()
+                mapObjCollection.addPlacemark(
+                    Point(cameraPositionTarget.latitude, cameraPositionTarget.longitude),
+                    ImageProvider.fromResource(applicationContext, R.drawable.ic_22)
+                )
+            }
+            R.id.targ23 -> {
+                val cameraPositionTarget = binding.mapView.map.cameraPosition.target
+                val mapObjCollection = binding.mapView.map.mapObjects.addCollection()
+                mapObjCollection.addPlacemark(
+                    Point(cameraPositionTarget.latitude, cameraPositionTarget.longitude),
+                    ImageProvider.fromResource(applicationContext, R.drawable.ic_23)
+                )
+            }
+            R.id.targ24 -> {
+                val cameraPositionTarget = binding.mapView.map.cameraPosition.target
+                val mapObjCollection = binding.mapView.map.mapObjects.addCollection()
+                mapObjCollection.addPlacemark(
+                    Point(cameraPositionTarget.latitude, cameraPositionTarget.longitude),
+                    ImageProvider.fromResource(applicationContext, R.drawable.ic_24)
+                )
+            }
+            R.id.targ25 -> {
+                val cameraPositionTarget = binding.mapView.map.cameraPosition.target
+                val mapObjCollection = binding.mapView.map.mapObjects.addCollection()
+                mapObjCollection.addPlacemark(
+                    Point(cameraPositionTarget.latitude, cameraPositionTarget.longitude),
+                    ImageProvider.fromResource(applicationContext, R.drawable.ic_25)
+                )
+            }
+            R.id.targ27 -> {
+                val cameraPositionTarget = binding.mapView.map.cameraPosition.target
+                val mapObjCollection = binding.mapView.map.mapObjects.addCollection()
+                mapObjCollection.addPlacemark(
+                    Point(cameraPositionTarget.latitude, cameraPositionTarget.longitude),
+                    ImageProvider.fromResource(applicationContext, R.drawable.ic_27)
+                )
+            }
+            R.id.targ29 -> {
+                val cameraPositionTarget = binding.mapView.map.cameraPosition.target
+                val mapObjCollection = binding.mapView.map.mapObjects.addCollection()
+                mapObjCollection.addPlacemark(
+                    Point(cameraPositionTarget.latitude, cameraPositionTarget.longitude),
+                    ImageProvider.fromResource(applicationContext, R.drawable.ic_29)
+                )
+            }
+            R.id.targ30 -> {
+                val cameraPositionTarget = binding.mapView.map.cameraPosition.target
+                val mapObjCollection = binding.mapView.map.mapObjects.addCollection()
+                mapObjCollection.addPlacemark(
+                    Point(cameraPositionTarget.latitude, cameraPositionTarget.longitude),
+                    ImageProvider.fromResource(applicationContext, R.drawable.ic_30)
+                )
+            }
+            R.id.targ31 -> {
+                val cameraPositionTarget = binding.mapView.map.cameraPosition.target
+                val mapObjCollection = binding.mapView.map.mapObjects.addCollection()
+                mapObjCollection.addPlacemark(
+                    Point(cameraPositionTarget.latitude, cameraPositionTarget.longitude),
+                    ImageProvider.fromResource(applicationContext, R.drawable.ic_31)
+                )
+            }
+            R.id.targ99 -> {
+                val cameraPositionTarget = binding.mapView.map.cameraPosition.target
+                val mapObjCollection = binding.mapView.map.mapObjects.addCollection()
+                mapObjCollection.addPlacemark(
+                    Point(cameraPositionTarget.latitude, cameraPositionTarget.longitude),
+                    ImageProvider.fromResource(applicationContext, R.drawable.ic_99)
+                )
+            }
+            R.id.breach -> {
+                val cameraPositionTarget = binding.mapView.map.cameraPosition.target
+                val mapObjCollection = binding.mapView.map.mapObjects.addCollection()
+                mapObjCollection.addPlacemark(
+                    Point(cameraPositionTarget.latitude, cameraPositionTarget.longitude),
+                    ImageProvider.fromResource(applicationContext, R.drawable.ic_breach)
+                )
+            }
+        }
+        return true
     }
     
     private fun setupBluetooth() {
         val bluetoothManager = getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
         val bluetoothAdapter = bluetoothManager.adapter
-        bluetoothConnection = BluetoothConnection(bluetoothAdapter,
-            context = this, listener = this)
-
+        bluetoothConnection = BluetoothConnection(
+            bluetoothAdapter,
+            context = this, listener = this
+        )
+        
         dialog = SelectBluetoothFragment(bluetoothAdapter, object : BluetoothCallback {
             override fun onClick(item: BluetoothListItem) {
                 item.let {
@@ -107,16 +275,16 @@ class MainActivity : AppCompatActivity(), BluetoothReceiver.MessageListener {
         })
     }
     
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
+    /* override fun onCreateOptionsMenu(menu: Menu): Boolean {
+         menuInflater.inflate(R.menu.main, menu)
+         return true
+     }*/
     
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-
+    
     private fun setupOptionsMenu() {
         val menuHost: MenuHost = this
         menuHost.addMenuProvider(object : MenuProvider {
@@ -176,7 +344,6 @@ class MainActivity : AppCompatActivity(), BluetoothReceiver.MessageListener {
             ) == PackageManager.PERMISSION_GRANTED
         ) {
             // when permission is already grant
-            Log.d("checkLocationPermission", "checkLocationPermission")
             checkGPS()
         } else {
             // when permission is denied
@@ -249,11 +416,11 @@ class MainActivity : AppCompatActivity(), BluetoothReceiver.MessageListener {
                     binding.mapView.map.move(
                         CameraPosition(
                             Point(location.latitude, location.longitude),
-                            11.0f,
+                            12.0f,
                             0.0f,
                             0.0f
                         ),
-                        Animation(Animation.Type.SMOOTH, 1f), null
+                        Animation(Animation.Type.SMOOTH, 1.0f), null
                     )
                 } catch (e: IOException) {
                 
